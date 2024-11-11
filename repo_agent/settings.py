30bf92ff90153007a9ea56023c32581a882c4fc7
cnm13ryan <channokman16@gmail.com>
2024-11-11 01:48:27 +0000

    build: qwen2.5 as base model for chat completion

--- repo_agent/settings.py
+++ repo_agent/settings.py
@@ -68,10 +68,10 @@ class ProjectSettings(BaseSettings):
 
 
 class ChatCompletionSettings(BaseSettings):
-    model: str = "gpt-3.5-turbo"
-    temperature: PositiveFloat = 0.2
-    request_timeout: PositiveFloat = 60.0
-    base_url: HttpUrl = "https://api.openai.com/v1"  # type: ignore
+    model: str = "qwen2.5:7b"
+    temperature: PositiveFloat = 0.5
+    request_timeout: PositiveFloat = 100.0
+    base_url: HttpUrl = "http://localhost:11434/v1"  # type: ignore
     openai_api_key: SecretStr = Field(..., exclude=True)
 
     @field_serializer("base_url")
@@ -92,6 +92,7 @@ if _config_data == {}:
 
 # NOTE Each model's token limit has been reduced by 1024 tokens to account for the output space and 1 for boundary conditions.
 max_input_tokens_map = {
+    "qwen2.5:7b": 128000, 
     "gpt-3.5-turbo": 4096,  # NOTE OPENAI said that The gpt-3.5-turbo model alias will be automatically upgraded from gpt-3.5-turbo-0613 to gpt-3.5-turbo-0125 on February 16th. But in 2/20, then still maintain 4,096 tokens for context window.
     "gpt-3.5-turbo-0613": 4096,  # NOTE Will be deprecated on June 13, 2024.
     "gpt-3.5-turbo-16k": 16384,  # NOTE Will be deprecated on June 13, 2024.
